401379f6dba1cba5fa4f81433c94117af7f7b6aa
“thisnabeel” <rockystorm@gmail.com>
2024-03-03 22:25:46 -0800

    Slides Maker

--- src/routes/quranflow/slides-maker/+page.svelte
+++ src/routes/quranflow/slides-maker/+page.svelte
@@ -12,64 +12,60 @@
 </script>
 
 <div class="wrapper">
-	<div class="row">
-		<div class="col-lg-6">
-			<h1>Start:</h1>
-			<select class="form-control" bind:value={selectedStartingSurah}>
-				{#each allSurahs() as surah}
-					<option value={surah.surah}>{surah.title}</option>
-				{/each}
-			</select>
+	<select class="form-control" bind:value={selectedStartingSurah}>
+		{#each allSurahs() as surah}
+			<option value={surah.surah}>{surah.title}</option>
+		{/each}
+	</select>
+	{#if selectedStartingSurah}
+		<div class="row">
+			<div class="col-lg-6">
+				<h1>Start:</h1>
 
-			{#if selectedStartingSurah}
-				<select class="form-control" bind:value={selectedStartingVerse}>
-					{#each findSurah(selectedStartingSurah).folder as verse}
-						<option value={verse}>{verse.split(':')[1]}</option>
-					{/each}
-				</select>
-			{/if}
-		</div>
-		<div class="col-lg-6">
-			<!--  -->
-			<h1>End:</h1>
-			<select class="form-control" bind:value={selectedEndingSurah}>
-				{#each allSurahs().filter((s) => s.surah >= selectedStartingSurah) as surah}
-					<option value={surah.surah}>{surah.title}</option>
-				{/each}
-			</select>
-
-			{#if selectedEndingSurah}
+				{#if selectedStartingSurah}
+					<select class="form-control" bind:value={selectedStartingVerse}>
+						{#each findSurah(selectedStartingSurah).folder as verse}
+							<option value={verse}>{verse.split(':')[1]}</option>
+						{/each}
+					</select>
+				{/if}
+			</div>
+			<div class="col-lg-6">
+				<!--  -->
+				<h1>End:</h1>
 				<select class="form-control" bind:value={selectedEndingVerse}>
-					{#each findSurah(selectedEndingSurah).folder as verse}
-						<option value={verse}>{verse.split(':')[1]}</option>
+					{#each findSurah(selectedStartingSurah).folder as verse}
+						{#if selectedStartingVerse && selectedStartingVerse.split(':')[1] <= verse.split(':')[1]}
+							<option value={verse}>{verse.split(':')[1]}</option>
+						{/if}
 					{/each}
 				</select>
-			{/if}
-		</div>
+			</div>
 
-		<div class="link">
-			<a
-				class="btn btn-lg btn-primary"
-				href={'http://natadarrab-svelte.vercel.app/quranflow/' +
-					selectedStartingVerse +
-					'-' +
-					selectedEndingVerse +
-					'/slides'}
-				target="_blank">{selectedStartingVerse} - {selectedEndingVerse}</a
-			>
-			<br /> <br />
-			<input
-				type="text"
-				class="form-control"
-				disabled
-				value={'http://natadarrab-svelte.vercel.app/quranflow/' +
-					selectedStartingVerse +
-					'-' +
-					selectedEndingVerse +
-					'/slides'}
-			/>
+			<div class="link">
+				<a
+					class="btn btn-lg btn-primary"
+					href={'http://natadarrab-svelte.vercel.app/quranflow/' +
+						selectedStartingVerse +
+						'-' +
+						selectedEndingVerse +
+						'/slides'}
+					target="_blank">{selectedStartingVerse} - {selectedEndingVerse}</a
+				>
+				<br /> <br />
+				<input
+					type="text"
+					class="form-control"
+					disabled
+					value={'http://natadarrab-svelte.vercel.app/quranflow/' +
+						selectedStartingVerse +
+						'-' +
+						selectedEndingVerse +
+						'/slides'}
+				/>
+			</div>
 		</div>
-	</div>
+	{/if}
 </div>
 
 <!-- 
